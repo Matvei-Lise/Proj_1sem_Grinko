0c18045d7e0091ba70e2ee3647430dd5be07d3c1
Matvei-Lise <89886662592@mail.ru>
2022-11-22 11:09:25 +0300

    2 задача 6го пз

---
+++ PZ_6/PZ_6_2.py
@@ -0,0 +1,10 @@
+#Дан список размера N. Найти номер его последнего локального максимума.
+
+import random
+
+def PrintList(List):
+    for element in List:
+        print(element, end=' ')
+    print()
+
+List = []
