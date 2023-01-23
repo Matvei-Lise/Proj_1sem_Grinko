9118cbafb1691e141c560329a0d87d74323273c4
Матвей <114809509+Matvei-Lise@users.noreply.github.com>
2023-01-23 22:53:06 +0300

    Update PZ_11_2.py

--- PZ_11/PZ_11_2.py
+++ PZ_11/PZ_11_2.py
@@ -3,3 +3,18 @@
 #Сформировать новый файл, в который поместить текст в стихотворной форме
 # предварительно заменив символы верхнего регистра на нижний.
 t = 0
+for i in open('text18-4.txt', encoding='UTF-8'):
+    print(i, end='')
+    for j in i:
+        if j == 'а' or 'б' or 'в' or 'г' or 'д' or 'е' or 'ж' or 'з' or 'и' or 'к' or 'л' or 'м' or 'н' or 'о' or 'п' or 'р' or 'с' or 'т' or 'у' or 'ф' or 'х' or 'ц' or 'ч' or 'ш' or 'щ' or 'ъ' or 'ы' or 'ь' or 'э' or 'ю' or 'я':
+            t += 1
+print(end='\n')
+print('Количество символов, принадлежащих к группе букв: ', t, end='\n')
+
+f1 = open('text18-4.txt', encoding='UTF-8')
+low = f1.read().lower()
+f1.close()
+
+f2 = open('text-end.txt', 'w')
+print(low, file=f2)
+f2.close()
