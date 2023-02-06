569845de460a61cf57f7f14f0e0615d38ded3531
Матвей <114809509+Matvei-Lise@users.noreply.github.com>
2023-02-06 21:55:45 +0300

    Update PZ_12_1.py

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -4,12 +4,14 @@ import random
 
 n = int(input("Введите количество элементов в последовательности: "))
 sequence = []
+
 t = 0
 while t < n:
     sequence.append(random.randint(-100, 100))
     t += 1
+print("Старый список:  ",sequence)
 mul = sequence[n-1]
-map(lambda x: x*mul, sequence)
+sequence = (list(map(lambda x: x*mul, sequence)))
 del sequence[n-1]
 sequence.append(mul)
-print(sequence)
+print("Новый список:  ", sequence)
