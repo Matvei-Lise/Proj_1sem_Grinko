3a4653aa9e5b9faa8c348723a9337faee8d1fb78
Матвей <114809509+Matvei-Lise@users.noreply.github.com>
2023-03-10 14:40:38 +0300

    Add files via upload

---
+++ PZ_16/main.py
@@ -0,0 +1,13 @@
+from PZ_16 import module
+
+module.get_set()
+
+module.get_txt()
+
+print(PZ_16.module.list_doc.doc)
+
+print(PZ_16.module.list_doc.doc)
+
+print(module.get_binary())
+
+print(dir(module))
